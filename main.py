9dfad5d73e3226925b27f4f87e16b5d83022928b
Robert Morgan <220232196+atascension@users.noreply.github.com>
2026-05-11 19:01:44 -0400

    feat: add image serving functionality and update photo placeholders in index.html

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/img/<filename>")
+def img_file(filename):
+    return send_from_directory('img', filename)
 
 if __name__ == "__main__":
     app.run()
